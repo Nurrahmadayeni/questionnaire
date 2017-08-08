0025fbd8edd9bfddf085b79f2b517ebbb49b24b8
nurrahmadayeni_ <nurrahmadayeni@students.usu.ac.id>
2017-08-08 09:19:53 +0700

    deleted unnecessary

--- array.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$stuff = array('orange','Banana', 'apples','Orange', 'Banana', 'banana', 'banana');
-
-	$c = array_count_values($stuff); 
-	print_r($c);
-	$val = array_search(max($c), $c);
-
-	echo "Terbanyak adalah ".$val;
-?>
--- arrayT.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-$array = array(0 => 'blue', 1 => 'red', 2 => 'green', 3 => 'red');
-
-$key = array_search(3, $array); // $key = 2;
-$key2 = array_search('red', $array);   // $key = 1;
-
-echo $key.' key 2 : '. $key2;
-?>
